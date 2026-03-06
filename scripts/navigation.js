9e8ab5e8f3d0e0e5fdaaacfedef3caf329c5c548
Caeser32 <tmsiza@byupathway.edu>
2026-03-07 01:46:27 +0200

    Completed Week 1 Course Home Page

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    hamburger.textContent = navMenu.classList.contains('show') ? '\u2715' : '\u2630';
+});
